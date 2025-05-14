b5cf419b662070cb9164634f531b3ee19a69ba12
Cho SeungYeon <111514472+layout-SY@users.noreply.github.com>
2025-05-14 16:22:33 +0900

    style : global 스타일 적용

---
+++ src/style/global.ts
@@ -0,0 +1,51 @@
+import 'sanitize.css';
+
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    background-color : #ccc;
+    margin: 0;
+    padding:0;
+    font-family: 'Pretendard';
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    box-sizing: border-box;
+  }
+  html {
+    font-family: 'Pretendard';
+  }
+  body {
+    font-family: 'Pretendard';
+
+  }
+  h1 {
+    margin: 0;
+  }
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+  button,
+  input,
+  select,
+  textarea {
+    background-color: transparent;
+    border: 0;
+    &:focus {
+      box-shadow: none;
+      outline: none;
+    }
+  }
+
+  a,
+  button {
+    cursor: pointer;
+  }
+
+  ul,
+  ol {
+    padding-left: 0;
+    list-style: none;
+  }
+`;
