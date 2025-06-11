d8b96609a0ea796b9be9d70210224b086a9b73f3
Cho SeungYeon <111514472+layout-SY@users.noreply.github.com>
2025-06-12 00:24:38 +0900

    feat : 회원가입의 RHF name 타입 정의

--- src/models/auth.ts
+++ src/models/auth.ts
@@ -1,4 +1,13 @@
 import type { z } from 'zod';
-import type { LoginScheme } from '../constants/authZodConstants';
+import type { LoginScheme, SignUpScheme } from '../constants/authZodConstants';
 
 export type LoginSchemeType = z.infer<typeof LoginScheme>;
+
+export type SignUpSchemeType = z.infer<typeof SignUpScheme>;
+
+export type SignUpSchemeNameType =
+  | 'email'
+  | 'password'
+  | 'confirmPassword'
+  | 'nickname'
+  | 'name';
